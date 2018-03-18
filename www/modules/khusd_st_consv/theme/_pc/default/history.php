45b79864565b500b2e9459b474f48b09b39239cc
wskim <kws015@hanmail.net>
2018-03-19 00:37:00 +0900

    보존과 수정 사항 1. history의 내용을 4학년 1학기에 맞게 나올 수 있도록 정리

--- www/modules/khusd_st_consv/theme/_pc/default/history.php
+++ www/modules/khusd_st_consv/theme/_pc/default/history.php
@@ -1,5 +1,7 @@
 <div id="consv_history" class="khusd_st list consv">
 
+       <?php $SEMESTER_INFO = getCurrentSemesterInfo()  ?>
+
 	<table summary="보존과 점수표 기록입니다.">
 	<caption>보존과 점수표 기록</caption> 
 	<colgroup> 
@@ -10,7 +12,9 @@
 	<col width="40"> 
 	<col width="40"> 
 	<col width="40"> 
+        <?php if($SEMESTER_INFO['sid'] == 2):?>
 	<col width="40"> 
+        <?php endif?>
 	<col width="40"> 
 	<col width="40"> 
 	<col width="40"> 
@@ -33,7 +37,12 @@
 	<tr>
 		<th rowspan="3" scope="col" class="split category1">No</th>
 		<th rowspan="3" scope="col" class="split category1">학번,이름</th>
+        <?php if($SEMESTER_INFO['sid'] == 2):?>
 		<th colspan="6" scope="col" class="split category2">[Endodontic Tx.]</th>
+        <?php endif?>
+        <?php if($SEMESTER_INFO['sid'] == 3):?>
+		<th colspan="5" scope="col" class="split category2">[Endodontic Tx.]</th>
+        <?php endif?>
 		<th colspan="13" scope="col" class="split category3">[일반]</th>
 		<th colspan="3" scope="col" class="split category4"></th>
 		<th rowspan="3" scope="col" class="category5">수정일</th>
@@ -41,7 +50,9 @@
 	<tr>
 		<th colspan="4" scope="col" class="split category2">obs.</th>
 		<th rowspan="2" scope="col" class="split category2">점수</th>
+        <?php if($SEMESTER_INFO['sid'] == 2):?>
 		<th rowspan="2" scope="col" class="split category2">PRe St(Endo)</th>
+        <?php endif?>
 		<th colspan="2" scope="col" class="split category3">Indirect</th>
 		<th rowspan="2" scope="col" class="split category3">AF</th>
 		<th colspan="3" scope="col" class="split category3">Tooth colored</th>
@@ -50,8 +61,17 @@
 		<th rowspan="2" scope="col" class="split category3">Sur</th>
 		<th rowspan="2" scope="col" class="split category3">Mis</th>
 		<th rowspan="2" scope="col" class="split category3">점수</th>
+                <?php if($SEMESTER_INFO['sid'] == 2):?>
+
 		<th rowspan="2" scope="col" class="split category3">Pre ST(AF)</th>
 		<th rowspan="2" scope="col" class="split category3">Pre ST(OP)</th>
+                <?php endif?>
+                <?php if($SEMESTER_INFO['sid'] == 3):?>
+
+                <th rowspan="2" scope="col" class="split category3">Pre ST(Inlay Gold)</th>
+                <th rowspan="2" scope="col" class="split category3">Pre ST(Inlay Resin)</th>
+                <?php endif?>
+
 		<th rowspan="2" scope="col" class="split category4">득점</th>
 		<th rowspan="2" scope="col" class="split category4">감점</th>
 		<th rowspan="2" scope="col" class="split category4">총점</th>
@@ -85,7 +105,9 @@
 		<td><?php echo $SCORE['endo_ant']?></td>
 		<td><?php echo $SCORE['endo_etc']?></td>
 		<td class="category3"><?php echo $SCORE['endo_score']?></td>
+        	<?php if($SEMESTER_INFO['sid'] == 2):?>
 		<td><?php echo ($SCORE['pre_st_endo'] == 1 ? '완' : '')?></td>
+                <?php endif?>
 		<td class="category1"><?php echo $SCORE['indirect_prep_imp']?></td>
 		<td class="category1"><?php echo $SCORE['indirect_setting']?></td>
 		<td class="category1"><?php echo $SCORE['am']?></td>
@@ -97,8 +119,18 @@
 		<td class="category2"><?php echo $SCORE['surgery']?></td>
 		<td class="category2"><?php echo $SCORE['miscellaneous']?></td>
 		<td class="category3"><?php echo $SCORE['op_score']?></td>
+                <?php if($SEMESTER_INFO['sid'] == 2):?>
+
 		<td><?php echo ($SCORE['pre_st_am'] == 1 ? '완' : '')?></td>
 		<td><?php echo ($SCORE['pre_st_op'] == 1 ? '완' : '')?></td>
+                <?php endif?>
+
+                <?php if($SEMESTER_INFO['sid'] == 3):?>
+
+        <td><?php echo ($SCORE['pre_st_inlay_gold'] == 1 ? '완' : ($SCORE['pre_st_inlay_gold_prep'].'/'.$SCORE['pre_st_inlay_gold_setting']))?></td>
+        <td><?php echo ($SCORE['pre_st_inlay_resin'] == 1 ? '완' : ($SCORE['pre_st_inlay_resin_prep'].'/'.$SCORE['pre_st_inlay_resin_setting']))?></td>
+                <?php endif?>
+
 		<td class="category4"><?php echo $SCORE['plus_score']?></td>
 		<td class="category4"><?php echo $SCORE['minus_score']?></td>
 		<td class="category4"><?php echo $SCORE['obser_score']?></td>
