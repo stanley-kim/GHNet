bc7345b2a8c9b7748d271af032227ba1a9eb681f
wskim <kws015@hanmail.net>
2019-01-21 22:54:17 +0900

    1. 바탕화면 Timeout 표시기능 추가 2. 바탕 색 변경 3. 바탕 이미지 안 쓰도록

--- www/layouts/mobile/_var/_var.php
+++ www/layouts/mobile/_var/_var.php
@@ -1,8 +1,10 @@
 <?php
-$d['layout']['title'] = "나의 모바일홈";
-$d['layout']['imglogo_use'] = "1";
+//$d['layout']['title'] = "4학년2학기";
+$d['layout']['title'] = "4학년2학기 (D".((strtotime(date('Y-m-d',time())) - strtotime("2019-01-18")) / 86400).")";
+
+$d['layout']['imglogo_use'] = "";
 $d['layout']['imglogo'] = "logo_white_only.png";
-$d['layout']['title_color'] = "#D96A6A";
+$d['layout']['title_color'] = "#0000ff";
 $d['layout']['mainType_layout'] = "";
 $d['layout']['mainType_rb'] = "1";
 $d['layout']['tmenu_1'] = "m1";
@@ -10,10 +12,10 @@ $d['layout']['tmenu_1_text'] = "신청";
 $d['layout']['tmenu_1_link'] = "./?c=1";
 $d['layout']['tmenu_2'] = "m2";
 $d['layout']['tmenu_2_text'] = "교정과 F/U";
-$d['layout']['tmenu_2_link'] = "./?c=8/11";
+$d['layout']['tmenu_2_link'] = "./?c=16/19";
 $d['layout']['tmenu_3'] = "m17";
 $d['layout']['tmenu_3_text'] = "보철과  F/U";
-$d['layout']['tmenu_3_link'] = "./?c=17/65";
+$d['layout']['tmenu_3_link'] = "./?c=25/81";
 $d['layout']['bbs1'] = "n";
 $d['layout']['sort1'] = "gid,asc";
 $d['layout']['bbs1_name'] = "Today's Story";
