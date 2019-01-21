4d7de49aef67c94b08fc8fe158726c87e549ab88
wskim <kws015@hanmail.net>
2019-01-21 22:51:13 +0900

    45기 default layout _var.php

---
+++ www/layouts/mobile/_var/_var.php
@@ -0,0 +1,37 @@
+<?php
+$d['layout']['title'] = "나의 모바일홈";
+$d['layout']['imglogo_use'] = "1";
+$d['layout']['imglogo'] = "logo_white_only.png";
+$d['layout']['title_color'] = "#D96A6A";
+$d['layout']['mainType_layout'] = "";
+$d['layout']['mainType_rb'] = "1";
+$d['layout']['tmenu_1'] = "m1";
+$d['layout']['tmenu_1_text'] = "신청";
+$d['layout']['tmenu_1_link'] = "./?c=1";
+$d['layout']['tmenu_2'] = "m2";
+$d['layout']['tmenu_2_text'] = "교정과 F/U";
+$d['layout']['tmenu_2_link'] = "./?c=8/11";
+$d['layout']['tmenu_3'] = "m17";
+$d['layout']['tmenu_3_text'] = "보철과  F/U";
+$d['layout']['tmenu_3_link'] = "./?c=17/65";
+$d['layout']['bbs1'] = "n";
+$d['layout']['sort1'] = "gid,asc";
+$d['layout']['bbs1_name'] = "Today's Story";
+$d['layout']['bbs1_namehide'] = "";
+$d['layout']['bbs1_num'] = "5";
+$d['layout']['bbs1_day'] = "30";
+$d['layout']['bbs2'] = "n";
+$d['layout']['sort2'] = "gid,asc";
+$d['layout']['bbs2_name'] = "Hot Photo";
+$d['layout']['bbs2_namehide'] = "";
+$d['layout']['bbs2_num'] = "4";
+$d['layout']['bbs2_day'] = "30";
+$d['layout']['bbs3'] = "n";
+$d['layout']['sort3'] = "gid,asc";
+$d['layout']['bbs3_name'] = "What's New?";
+$d['layout']['bbs3_namehide'] = "";
+$d['layout']['bbs3_num'] = "10";
+$d['layout']['bbs3_day'] = "30";
+$d['layout']['headerfix'] = "";
+$d['layout']['begin'] = "1";
+?>
